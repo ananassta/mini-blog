28b061d77e1f519734131c1d1f831e079596cb82
Anastasiya Bartashuk <55900056+ananassta@users.noreply.github.com>
2022-05-25 23:08:35 +0300

    Add files via upload

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,8 +1,13 @@
+import { getAllPostsFiltered } from '.'
 import { getSortedPostsData } from '../../lib/posts'
 
+
+// const posts = process.env.NODE_ENV === 'production' ? require('../../cache/data').posts : getAllPostsFiltered()
 const posts = process.env.NODE_ENV === 'production' ? require('../../cache/data').posts : getSortedPostsData()
 
 export default (req, res) => {
+  // let posts = getAllPostsFiltered();
+  console.log(posts.filter(p => p!=''));
   const results = req.query.q ?
     posts.filter(post => post.title.toLowerCase().includes(req.query.q)) : []
   res.statusCode = 200
